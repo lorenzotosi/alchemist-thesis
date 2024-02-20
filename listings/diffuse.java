5df121f0332079a8a63be2520f50998c4426cecc
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-02-21 00:35:13 +0100

    change code

--- listings/diffuse.java
+++ listings/diffuse.java
@@ -3,14 +3,10 @@ public class Diffuse<T, P extends Position<P> & Position2D<P>> extends AbstractG
     @Override
     public void action(final PheromoneLayerImpl<P> phLayer) {
         var pheromoneMap = phLayer.getPheromoneMap();
-        var nodePositionList = getEnvironment().getNodes()
-                                                .stream()
-                                                .map(a -> getEnvironment().getPosition(a)).toList();
-        nodePositionList.forEach(p -> {
-            P patchPos = phLayer.adaptPosition(p);
-            Double phValue = pheromoneMap.getOrDefault(nPos, 0.0);
-            if (phValue > customDiffusionThreshold)
-                getNeighborhood(patchPos).forEach(x -> phLayer.deposit(x, phValue * diffusionValue));
+        pheromoneMap.forEach((k, v) -> {
+            if (v > customDiffusionThreshold){
+                getNeighborhood(k).forEach(x -> phLayer.deposit(x, v * diffusionValue));
+            }
         });
     }
     ...
